539821b1add6eab38283d67cdcfe100d83b37f76
JosepAntoni <costacamps8@gmail.com>
2018-11-24 11:40:22 +0100

    .po created

--- Main.py
+++ Main.py
@@ -1,24 +1,32 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import gettext
 import time
+import os
+
+gettext.install("Main", "locales")
+
 
 if __name__ == '__main__':
     cadena = None
-    print ("Hola")
-    print ("Bienvenido a mi juego")
-    print ("Empezemos por crear tu heroe")
-    print ("Introduce el nombre de tu heroe")
+    print _("Hola")
+    print _("Bienvenido a mi juego")
+    print _("Empezemos por crear tu heroe")
+    print _("Introduce el nombre de tu heroe")
     name = raw_input()
-    print ("Introduce el color de cabello de tu heroe")
+    print _("Introduce el color de cabello de tu heroe")
     hair = raw_input()
-    print ("Introduce la edad de tu heroe")
+    print _("Introduce la edad de tu heroe")
     age = raw_input()
-    print ("Vamos a mostrar tu heroe")
-    print ("Edad: ", hair)
-    print ("Nombre: ", age)
-    print ("Cabello: ", name)
-    print ("Es correcto? (s/n)")
+    print _("Vamos a mostrar tu heroe")
+    print _("Edad: ", hair)
+    print _("Nombre: ", age)
+    print _("Cabello: ", name)
+    print _("Es correcto? (s/n)")
     raw_input()
-    print ("Empezemos")
+    print _("Empezemos")
     time.sleep(1)
     print ("3")
     time.sleep(1)
@@ -26,8 +34,8 @@ if __name__ == '__main__':
     time.sleep(1)
     print (1)
     time.sleep(1)
-    print ("START")
+    print _("START")
     time.sleep(1)
-    print ("Tu heroe muere, el mundo sucumbe en el caos y PHP lo domina")
+    print _("Tu heroe muere, el mundo sucumbe en el caos y PHP lo domina")
     time.sleep(3)
-    print ("Adios")
+    print _("Adios")
